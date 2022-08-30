005d5f306f5ab63af4dddec7f0d18c7a8fe2d1e1
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-08-30 23:05:09 +0000

    comitting both these codes

--- nameInverter.js
+++ nameInverter.js
@@ -1,6 +1,37 @@
 const nameInverter = function(name) {
+    name=name.trim()
+   
+    const nameSpilts=name.split(' ');
+    //['Dr.']
+    if(name===undefined){
+        return 'throw Error'
+    }
+    if (!name) {
+        return "";
+      }
+    
+      if(nameSpilts.length===1 && nameSpilts[0]==='Dr.'){
+        return "";
+      };
+      
+      if(nameSpilts.length===2 && nameSpilts[0]==='Dr.' ){
+        return name;
+      };
+      if(nameSpilts.length===3 && nameSpilts[0]==='Dr.' ){
+        return `${nameSpilts[0]} ${nameSpilts[2]}, ${nameSpilts[1]}`;
+      };
 
-};
-
-
-module.exports = nameInverter;
+    if(nameSpilts.length === 1){
+        return name;
+     }
+     
+     if(nameSpilts.length >=2){
+        
+        return nameSpilts.reverse().join(", ");
+     };
+     
+      //name=name.spilt();
+      //console.log(name)
+    };
+//console.log()
+module.exports=nameInverter;
